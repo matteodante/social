bc2c2e5bd4f7d6309f28874386dfc9fa833d1608
Matteo Dante <matteo.dante659@gmail.com>
2023-01-24 17:16:00 +0100

    Init

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -5,11 +5,6 @@ import Timeline from '@/Components/Timeline.vue';
 
 <template>
     <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
